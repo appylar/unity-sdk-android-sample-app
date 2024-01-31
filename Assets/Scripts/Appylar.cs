df68d3b7ac30a6ddef3e9cc879b1f87772017f53
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2024-01-31 20:47:46 +0100

    chore: format and gitignore

--- Assets/Scripts/Appylar.cs
+++ Assets/Scripts/Appylar.cs
@@ -3,90 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AppylarSdkWrapper;
 
-public class AppylarSample : MonoBehaviour, AppylarInitializationListener, AppylarInsterstitialListener, AppylarBannerListener
-{
-    private string appKey;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Initialize();
-    }
+public class AppylarSample: MonoBehaviour, AppylarInitializationListener, AppylarInsterstitialListener, AppylarBannerListener {
+  private string appKey;
+  // Start is called before the first frame update
+  void Start() {
+    Initialize();
+  }
 
-    public void Initialize()
-    {
-        Appylar.Initialize(
-            GetAppKey()
-        , new AdType[]{
-                AdType.INTERSTITIAL, AdType.BANNER
-            }
-            , true
-            , this
-            );
-    }
+  public void Initialize() {
+    Appylar.Initialize(
+      GetAppKey(), new AdType[] {
+        AdType.INTERSTITIAL, AdType.BANNER
+      }, true, this
+    );
+  }
 
-    private string GetAppKey()
-    {
-        if (Application.platform == RuntimePlatform.Android)
-        {
-            return appKey = "<appKeyAndroid>";
-        }
-        else
-        {
-            return appKey = "<appKeyIos>";
-        }
+  private string GetAppKey() {
+    if (Application.platform == RuntimePlatform.Android) {
+      return appKey = "<appKeyAndroid>";
+    } else {
+      return appKey = "<appKeyIos>";
     }
+  }
 
-    public void Interstitial()
-    {
-        Appylar.ShowInterstitial(" ", this);
-    }
+  public void Interstitial() {
+    Appylar.ShowInterstitial(" ", this);
+  }
 
-    public void ShowBanner()
-    {
-        Appylar.ShowBanner(BannerPosition.BOTTOM, " ", this);
-    }
+  public void ShowBanner() {
+    Appylar.ShowBanner(BannerPosition.BOTTOM, " ", this);
+  }
 
-    public void HideBanner()
-    {
-        Appylar.HideBanner();
-    }
+  public void HideBanner() {
+    Appylar.HideBanner();
+  }
 
-    #region Interface Implementations
-    public void onNoBanner()
-    {
-        print("onNoBanner");
-    }
+  #region Interface Implementations
+  public void onNoBanner() {
+    print("onNoBanner");
+  }
 
-    public void onBannerShown(int height)
-    {
-        print("onBannerShown " + height);
-    }
+  public void onBannerShown(int height) {
+    print("onBannerShown " + height);
+  }
 
-    public void onInitialized()
-    {
-        print("onInitialized");
-    }
+  public void onInitialized() {
+    print("onInitialized");
+  }
 
-    public void onError(string message)
-    {
-        print($"onError : {message}");
-    }
+  public void onError(string message) {
+    print($"onError : {message}");
+  }
 
-    public void onNoInterstitial()
-    {
-        print("onNoInterstitial ");
-    }
+  public void onNoInterstitial() {
+    print("onNoInterstitial ");
+  }
 
-    public void onInterstitialShown()
-    {
-        print("onInterstitialShown");
-    }
+  public void onInterstitialShown() {
+    print("onInterstitialShown");
+  }
 
-    public void onInterstitialClosed()
-    {
-        print("onInterstitialClosed ");
-    }
-    #endregion
-
+  public void onInterstitialClosed() {
+    print("onInterstitialClosed ");
+  }
+  #endregion
 
 }
