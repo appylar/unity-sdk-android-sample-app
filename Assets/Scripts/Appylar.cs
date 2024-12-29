1c5267f64b90031764bdaf15c3d20b082327dfed
toulouse88 <81014113+toulouse88@users.noreply.github.com>
2024-12-29 16:49:12 +0100

    Added status text

--- Assets/Scripts/Appylar.cs
+++ Assets/Scripts/Appylar.cs
@@ -7,16 +7,19 @@ using TMPro;
 public class AppylarSample: MonoBehaviour, AppylarInitializationListener, AppylarInterstitialListener, AppylarBannerListener {
 
   public TextMeshProUGUI textMeshProUGUI;
-
+ 
   // Start is called before the first frame update
   void Start() {
+    if (textMeshProUGUI == null){
+      textMeshProUGUI = GameObject.Find("status_text").GetComponent<TextMeshProUGUI>();
+      textMeshProUGUI.text = "Initializing the SDK, please wait...";
+    }
     Initialize();
-    textMeshProUGUI.text = "GABOR";
   }
 
   public void Initialize() {
     Appylar.Initialize(
-      "<YOUR_ANDROID_APP_KEY>", new AdType[] {
+      "<YOUR_UNITY_APP_KEY>", new AdType[] {
         AdType.INTERSTITIAL, AdType.BANNER
       }, true, this
     );
@@ -37,30 +40,37 @@ public class AppylarSample: MonoBehaviour, AppylarInitializationListener, Appyla
   #region Interface Implementations
   public void onNoBanner() {
     print("onNoBanner");
+    textMeshProUGUI.text = "No more banners in the buffer,\nplease retry again after a minute.";
   }
 
   public void onBannerShown(int height) {
     print("onBannerShown " + height);
+    textMeshProUGUI.text = "";
   }
 
   public void onInitialized() {
     print("onInitialized");
+    textMeshProUGUI.text = "The SDK is initialized.";
   }
 
   public void onError(string message) {
     print($"onError : {message}");
+    textMeshProUGUI.text = $"onError : {message}";
   }
 
   public void onNoInterstitial() {
     print("onNoInterstitial ");
+    textMeshProUGUI.text = "No more interstitials in the buffer,\nplease retry again after a minute.";
   }
 
   public void onInterstitialShown() {
     print("onInterstitialShown");
+    textMeshProUGUI.text = "";
   }
 
   public void onInterstitialClosed() {
     print("onInterstitialClosed ");
+    textMeshProUGUI.text = "";
   }
   #endregion
 
